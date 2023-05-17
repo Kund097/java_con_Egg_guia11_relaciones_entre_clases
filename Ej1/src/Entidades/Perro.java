4d91d651eee94f95ef255b016bed59e5a5c0f56c
Facundo Vallejos <Facund097dv@gmail.com>
2023-05-17 19:02:07 -0300

    ejercicio 1

--- Ej1/src/Entidades/Perro.java
+++ Ej1/src/Entidades/Perro.java
@@ -1,5 +1,7 @@
 package Entidades;
 
+import Enumeraciones.RazaPerro;
+
 /**
  *
  * @author facun
@@ -7,10 +9,55 @@ package Entidades;
 public class Perro {
     
     private String nombre;
-    private String raza;
+    private RazaPerro raza;
     private int edad;
     private String tamanio;
-    
-    
+
+    public Perro() {
+    }
+
+    public Perro(String nombre, RazaPerro raza, int edad, String tamanio) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.edad = edad;
+        this.tamanio = tamanio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public RazaPerro getRaza() {
+        return raza;
+    }
+
+    public void setRaza(RazaPerro raza) {
+        this.raza = raza;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(String tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    @Override
+    public String toString() {
+        return "Perro{" + "nombre=" + nombre + ", raza=" + raza + ", edad=" + edad + ", tamanio=" + tamanio + '}';
+    }
     
 }
--- Ej1/src/ej1/Ej1_main.java
+++ Ej1/src/ej1/Ej1_main.java
@@ -1,12 +1,18 @@
-
 package ej1;
-
 
 public class Ej1_main {
 
-   
+    /**
+     * Ahora deberemos en el main crear dos Personas y dos Perros. Después,
+     * vamos a tener que pensar la lógica necesaria para asignarle a cada
+     * Persona un Perro y por ultimo, mostrar desde la clase Persona, la
+     * información del Perro y de la Persona.
+     *
+     * @param args
+     */
+
     public static void main(String[] args) {
-        
+
     }
-    
+
 }
